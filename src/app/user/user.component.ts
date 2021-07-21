78e94c1e5a12cfd26a04a2250130a89389536388
sravankuruma <sravankuruma007@gmail.com>
2021-07-21 15:40:22 +0530

    Assignment 2

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+ 
+  username: string;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+  resetUsername(event :Event) {
+    this.username = '';
+  }
+}
